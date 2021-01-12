8d5ce5057495558989000164af0a8ca8793cd385
Economicsnerd <idrissamraoui@protonmail.com>
2021-01-12 09:17:42 -0500

    Added Navbar and Log Comps

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
+import Navbar from "./components/layouts/Navbar";
+import Logs from "./components/logs/Logs";
+
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
@@ -8,7 +11,14 @@ const App = () => {
     // Initializes Materialize JS
     M.AutoInit();
   });
-  return <div className="App">My App</div>;
+  return (
+    <Fragment>
+      <Navbar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
