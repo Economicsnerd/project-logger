d846ebb5f5ff96b98e564b8fe4db1ff0bc13dd41
Economicsnerd <idrissamraoui@protonmail.com>
2021-01-13 09:25:55 -0500

    AddBtn & AddLogModal Comps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, Fragment } from "react";
 import Navbar from "./components/layouts/Navbar";
 import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layouts/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -15,6 +17,8 @@ const App = () => {
     <Fragment>
       <Navbar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </Fragment>
