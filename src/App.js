81cfc7255ce0c8125d27d2456ba33c1fe537645d
Economicsnerd <idrissamraoui@protonmail.com>
2021-02-04 07:38:03 -0500

    Added Redux Store and Provider Setup

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
 import AddDevModal from "./components/devs/AddDevModal";
 import DevListModal from "./components/devs/DevListModal";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -17,17 +19,19 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <Fragment>
-      <Navbar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddDevModal />
-        <DevListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddDevModal />
+          <DevListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
